27d29bc69d598eb80b60546387c3de87c7fe6dae
paramagicdev <konnor5456@gmail.com>
2020-01-20 01:15:49 -0500

    add eslint, mess around w/ mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,11 @@
-import React from "react"
-import { Link } from "gatsby"
+---
+name: Hello world
+---
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { Button } from './Button'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+# Hello world
 
-export default IndexPage
+Hello, I'm still a mdx file, but now I have a button component !
+
+<Button onClick={() => { alert("You clicked me"); }}>Click me</Button>
