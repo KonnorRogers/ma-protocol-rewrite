7d4b2e3ed5c5d9d64d06558c4225d75f74d1f321
paramagicdev <konnor5456@gmail.com>
2020-01-23 00:44:14 -0500

    added menus

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,15 @@
 export default {
   // Configuring the left sidebar
-  // menu: [
-  // ]
+  menu: [
+    "Section 1: General Patient Care",
+    "Section 2: Medical Protocols",
+    "Section 3: Cardiac Emergencies",
+    "Section 4: Trauma Protocols",
+    "Section 5: Airway Protocols & Procedures",
+    "Section 6: Medical Director Options",
+    "Section 7: Medical Policies & Procedures",
+    "Section 8: Special Operations Principles",
+    "Appendices",
+  ],
   files: "./src/pages/**/*.{md,markdown,mdx}",
 }
