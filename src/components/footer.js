9c6136c7ea079f6821cfb2c987466802b26f33ff
paramagicdev <konnor5456@gmail.com>
2020-01-30 07:07:54 -0500

    adding styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import { css, jsx } from "@emotion/core"
+import { css } from "@emotion/core"
 import Screen from "../utils/screen.js"
 
 const Footer = () => {
---
+++ src/styles/headerCSS.js
@@ -0,0 +1,10 @@
+import { css } from "@emotion/core"
+
+const HeaderCSS = () => {
+  return css`
+    margin-top: 2rem;
+    margin-bottom: 0.75rem;
+  `
+}
+
+export default HeaderCSS
