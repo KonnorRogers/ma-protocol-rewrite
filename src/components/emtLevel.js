bbf45364857827d1d60575271d75e2b70ee3ebd1
paramagicdev <konnor5456@gmail.com>
2020-02-07 17:37:56 -0500

    update emtLevel.js

--- src/components/emtLevel.js
+++ src/components/emtLevel.js
@@ -1,9 +1,10 @@
+import React from "react"
 import { css } from "@emotion/core"
 import Screen from "../utils/screen.js"
 
 const colors = {
   basic: "green",
-  advanced: "orange",
+  advanced: "#ffbc0d",
   paramedic: "red",
 }
 
@@ -14,31 +15,50 @@ const capitalize = level => {
   return firstLetter + restOfWord
 }
 
+const emtHeaderCSS = level => css`
+  margin: 0;
+  font-size: 1.2rem;
+  color: ${colors[level]};
+
+  ${Screen.small} {
+    font-size: 1.3rem;
+  }
+`
+
+const linkAnchorCSS = level => css`
+  color: ${colors[level]};
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
 const emtLevelCSS = level => css`
-  margin: 2rem auto 4rem auto;
+  margin: 0.5rem auto 2rem auto;
   padding: 0.5rem 0.75rem;
   border: 1.5px solid ${colors[level]};
   border-radius: 4px;
   font-size: 1.15rem;
-
-  &::before {
-    content: "EMT-${capitalize(level)}";
-    position: absolute;
-    top: -2rem;
-    color: ${colors[level]}
-  }
 
   ${Screen.small} {
     font-size: 1.25rem;
   }
 `
 
-const EmtLevel = ({ children, level }) => (
-  <div className="emt-level-wrapper" style={{ position: "relative" }}>
-    <div className={level} css={emtLevelCSS(level)}>
-      {children}
-    </div>
-  </div>
-)
+const EmtLevel = ({ children, level }) => {
+  return (
+    <>
+      <h2 id={level} css={emtHeaderCSS(level)}>
+        <a css={linkAnchorCSS(level)} href={`#${level}`}>
+          {`EMT-${capitalize(level)}`}
+        </a>
+      </h2>
+      <section className={level} css={emtLevelCSS(level)}>
+        {children}
+      </section>
+    </>
+  )
+}
 
 export default EmtLevel
