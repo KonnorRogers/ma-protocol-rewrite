f04c3985d342d0cee46e7b97df2e6f8fdc69ddd3
paramagicdev <konnor5456@gmail.com>
2020-02-12 02:17:06 -0500

    trying to update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
 import 'src/styles/global.css'
+
+import React from "react";
+import ReactDOM from 'react-dom'
+import Title from "./src/components/title.js"
+
+export const onRouteChange = () => {
+  const mainContainer = document.querySelector(`[data-testid="main-container"]`);
+  const titleDiv = document.createElement('div');
+  const titleComponent = <Title title={document.title} />
+
+  mainContainer.insertAdjacentElement('afterbegin', titleDiv);
+  ReactDOM.render(titleComponent, titleDiv);
+}
--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -2,12 +2,16 @@ import * as React from 'react'
 import { Helmet } from 'react-helmet-async'
 import Footer from "../components/footer.js"
 
+// const mainContainer = document.querySelector(`[data-testid="main-container"]`);
+// const hi = <div>Hi</div>;
+// console.log(mainContainer);
+
+
 export default ({ children, doc }) => (
   <>
     <Helmet>
         <meta charSet="utf-8" />
         <title>{doc.value.name}</title>
-        />
     </Helmet>
     <main>
       {children}
