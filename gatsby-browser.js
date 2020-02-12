acd4ba964ff7a89ab64788b0940fd36aded52550
paramagicdev <konnor5456@gmail.com>
2020-02-12 15:11:34 -0500

    remove main container, go backto previous hacky method

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,16 @@
 import 'src/styles/global.css'
+
+const title = document.querySelector('[data-testid="page-title"]');
+const mainContainer = document.querySelector('[data-testid="main-container"]');
+
+function getTitle() {
+  return document.querySelector('[data-testid="page-title"]');
+}
+
+function getMainContainer() {
+  return document.querySelector('[data-testid="main-container"]');
+}
+
+export const onRouteUpdate = () => {
+  getMainContainer().insertAdjacentElement('afterbegin', getTitle());
+}
--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import Footer from "../components/footer.js"
 import PageNav from "../components/pageNav.js"
+import Title from "../components/title.js"
 
 export default ({ children, doc }) => (
   <>
     <main>
       {/* <PageNav data-testid="page-nav" /> */}
+      <Title data-testid="page-title" />
       {children}
       <Footer />
     </main>
