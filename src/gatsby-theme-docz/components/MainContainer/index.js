2c9b4f73cfaedfe06f0f813b47d8793b3411c19c
paramagicdev <konnor5456@gmail.com>
2020-02-19 12:23:55 -0500

    upgrade everything

---
+++ src/gatsby-theme-docz/components/MainContainer/index.js
@@ -0,0 +1,10 @@
+// import React from "react"
+
+// export const MainContainer = ({ children, ...rest }) => {
+//   return (
+//     <div>
+//       <h1> Hi </h1>
+//       {children}
+//     </div>
+//   )
+// }
--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -4,46 +4,44 @@ import PageNav from "../components/pageNav.js"
 import Title from "../components/title.js"
 
 export default ({ children }) => {
-  // function getTitle() {
-  //   return document.querySelector('[data-testid="page-title"]')
-  // }
+  function getTitle() {
+    return document.querySelector('[data-testid="page-title"]')
+  }
 
-  // function getTopPageNav() {
-  //   return document.querySelector('[data-testid="top-page-nav"]')
-  // }
+  function getTopPageNav() {
+    return document.querySelector('[data-testid="top-page-nav"]')
+  }
 
-  // function getBottomPageNav() {
-  //   return document.querySelector('[data-testid="bottom-page-nav"]')
-  // }
+  function getBottomPageNav() {
+    return document.querySelector('[data-testid="bottom-page-nav"]')
+  }
 
-  // function getMainContainer() {
-  //   return document.querySelector('[data-testid="main-container"]')
-  // }
+  function getMainContainer() {
+    return document.querySelector('[data-testid="main-container"]')
+  }
 
-  // // Equivalent to componentDidMount / componentDidUpdate
-  // useEffect(() => {
-  //   const mainContainer = getMainContainer()
+  // Equivalent to componentDidMount / componentDidUpdate
+  useEffect(() => {
+    const mainContainer = getMainContainer()
 
-  //   if (window.location.pathname !== "/") {
-  //     mainContainer.insertAdjacentElement("afterbegin", getTitle())
-  //   } else {
-  //     // Remove the title if on the homepage
-  //     getTitle().parentNode.removeChild(getTitle())
-  //   }
+    if (window.location.pathname !== "/") {
+      mainContainer.insertAdjacentElement("afterbegin", getTitle())
+    } else {
+      // Remove the title if on the homepage
+      getTitle().parentNode.removeChild(getTitle())
+    }
 
-  //   mainContainer.insertAdjacentElement("afterbegin", getTopPageNav())
-  //   mainContainer.insertAdjacentElement("beforeend", getBottomPageNav())
-  // }, children)
+    mainContainer.insertAdjacentElement("afterbegin", getTopPageNav())
+    mainContainer.appendChild(getBottomPageNav())
+  }, [])
 
   return (
     <>
-      <main>
-        {/* <PageNav data-testid="top-page-nav" /> */}
-        {/* <PageNav data-testid="bottom-page-nav" /> */}
-        {/* <Title data-testid="page-title" /> */}
-        {children}
-        <Footer />
-      </main>
+      <PageNav data-testid="top-page-nav" />
+      <Title data-testid="page-title" />
+      <PageNav data-testid="bottom-page-nav" />
+      {children}
+      <Footer />
     </>
   )
 }
