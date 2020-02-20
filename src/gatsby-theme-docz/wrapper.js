1651682be797a2c980a880823ac16179bbc2867e
paramagicdev <konnor5456@gmail.com>
2020-02-20 17:06:13 -0500

    trying to fix layout issues, will attempt upgrade

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -33,7 +33,7 @@ export default ({ children }) => {
 
     mainContainer.insertAdjacentElement("afterbegin", getTopPageNav())
     mainContainer.appendChild(getBottomPageNav())
-  }, children)
+  }, [])
 
   return (
     <>
