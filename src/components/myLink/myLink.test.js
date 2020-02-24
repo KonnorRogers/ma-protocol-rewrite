304d7d5b8336e6163be3b74a9ab350a9402c6a46
paramagicdev <konnor5456@gmail.com>
2020-02-24 12:31:51 -0500

    ' fix init

--- src/components/myLink/myLink.test.js
+++ src/components/myLink/myLink.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react"
 import MyLink from "./myLink.js"
 
 test("Renders without error", () => {
-  const tree = render(<MyLink />)
+  const tree = render(<MyLink to="/" />)
 
   expect(tree).toMatchSnapshot()
 })
--- src/components/note/note.test.js
+++ src/components/note/note.test.js
@@ -3,6 +3,6 @@ import { render } from "@testing-library/react"
 import Note from "./note.js"
 
 test("Renders without error", () => {
-  const tree = render(<Note />)
+  const tree = render(<Note id="test-note" />)
   expect(tree).toMatchSnapshot()
 })
