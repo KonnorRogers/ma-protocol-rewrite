30860fa14b50a1be6a3204ef574e397ac7988731
paramagicdev <konnor5456@gmail.com>
2020-02-26 09:53:12 -0500

    fix how snapshots are created

--- src/components/caution/caution.test.js
+++ src/components/caution/caution.test.js
@@ -4,5 +4,13 @@ import Caution from "./caution.js"
 
 test("Renders without error", () => {
   const { asFragment } = render(<Caution />)
-  expect(asFragment).toMatchSnapshot()
+  expect(asFragment()).toMatchSnapshot()
+})
+
+test("Renders with different props", () => {
+  const { asFragment, getByTestId } = render(
+    <Caution text="My caution" borderColor="green" data-testid="caution" />
+  )
+  expect(asFragment()).toMatchSnapshot()
+  expect(getByTestId("caution")).toBeTruthy()
 })
