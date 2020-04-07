e9152389c03e9b1c9e4ea879ecb1221c194ab03a
paramagicdev <konnor5456@gmail.com>
2020-04-07 16:06:38 -0400

    adding pain scale image

--- src/components/painScaleImg/index.js
+++ src/components/painScaleImg/index.js
@@ -7,7 +7,7 @@ const PainScaleImg = () => {
     query {
       placeholderImage: file(relativePath: { eq: "pain-scale.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 1365) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -15,7 +15,23 @@ const PainScaleImg = () => {
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return (
+    <div>
+      <Img
+        fluid={data.placeholderImage.childImageSharp.fluid}
+        alt="Pain Scale 1-10 with faces to depict pain"
+      />
+      <span style={{ textAlign: "center", fontSize: "0.85rem" }}>
+        Image taken from:
+        <a
+          style={{ display: "inline" }}
+          href="https://creakyjoints.org/doctor-patient/pain-scale-not-best-way-communicate-pain/"
+        >
+          https://creakyjoints.org/doctor-patient/pain-scale-not-best-way-communicate-pain/{" "}
+        </a>
+      </span>
+    </div>
+  )
 }
 
 export default PainScaleImg
