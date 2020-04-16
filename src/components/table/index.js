5eae2c61a27af52b8918ac16018351df04c99eb8
paramagicdev <konnor5456@gmail.com>
2020-04-16 10:47:30 -0400

    tables work againgaagp

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -3,16 +3,11 @@ import * as styles from "./styles.js"
 import PropTypes from "prop-types"
 import { useColorMode } from "theme-ui"
 // import { useLastChild } from "../../hooks/useLastChild"
-import { useTableColumns } from "../../hooks/useTableColumns"
+// import { useTableColumns } from "../../hooks/useTableColumns"
 
 function Table({ children, minWidth, colormode, ...rest }) {
   let colorMode = useColorMode()[0]
 
-  const tableColumnCount = useTableColumns(children)
-  const [tableColumns, setTableColumns] = React.useState(tableColumnCount)
-  console.log(tableColumnCount)
-
-  // Used to override the useColorMode hook for specific circumstances
   if (colormode) {
     colorMode = colormode
   }
@@ -43,10 +38,16 @@ export function TableRow({
   ...rest
 }) {
   const colorMode = colormode || useColorMode()[0]
+  const [columnCount, setColumnCount] = React.useState(0)
+
+  const count = React.Children.count(children)
+  const updatedChildren = React.Children.map(children, (child) => {
+    return React.cloneElement(child, { count: count })
+  })
 
   return (
     <div css={[styles.row({ lastRow, heading, colorMode }), css]} {...rest}>
-      {children}
+      {updatedChildren}
     </div>
   )
 }
@@ -63,7 +64,8 @@ TableRow.defaultProps = {
 
 export function TableItem({
   colspan,
-  tableColumns,
+  // tableColumns,
+  count,
   children,
   align,
   lastItem,
@@ -71,7 +73,9 @@ export function TableItem({
   ...rest
 }) {
   // hack
-  const columns = tableColumns
+  // const columns = tableColumns
+  console.log(count)
+  const columns = count
 
   const colorMode = colormode || useColorMode()[0]
 
