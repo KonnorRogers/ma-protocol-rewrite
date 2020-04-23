2184ad870f11fc0aa2f758f5a79e6783d2e26047
paramagicdev <konnor5456@gmail.com>
2020-04-23 17:29:20 -0400

    updating 2-17A sepsis'

--- src/components/caution/index.js
+++ src/components/caution/index.js
@@ -2,6 +2,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import InformationBox from "../informationBox"
 
+/**
+ * Caution component
+ */
 const Caution = ({ children, borderColor, text, alignment, ...rest }) => {
   return (
     <InformationBox
